e2beea60a1f709fd7ed06b24644b7bf6406a8c32
Maicon <andrade.maicon.pr@gmail.com>
2022-10-26 23:09:19 -0300

    chore: setup codebank db

---
+++ codebank/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	repository "github.com/AndradeMaicon/code-bank/infra/respository"
+	"github.com/AndradeMaicon/code-bank/usecase"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	db := setupDb()
+	defer db.Close()
+}
+
+func setupTransactionUseCase(db *sql.DB) usecase.UseCaseTransaction {
+	transactionRepository := repository.NewTransactionRepositoryDb(db)
+	useCase := usecase.NewUseCaseTransaction(transactionRepository)
+	return useCase
+}
+
+func setupDb() *sql.DB {
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("host"),
+		os.Getenv("port"),
+		os.Getenv("user"),
+		os.Getenv("password"),
+		os.Getenv("dbname"),
+	)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Fatal("error connection to database")
+	}
+	return db
+}
